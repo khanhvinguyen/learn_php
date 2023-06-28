85a7ef513984f1a389ba04935562f4db1219483e
khanhvinguyen <132880086+khanhvinguyen@users.noreply.github.com>
2023-06-28 19:48:16 +0700

    Add files via upload

---
+++ helper.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function redirect($url) {
+    header("Location: $url");
+    exit;
+}
+
+
+function redirectHome() {
+    redirect('index.php');
+}
+
+
---
+++ update.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'pdo.php';
+require_once 'helper.php';
+
+
+$request = $_POST;
+
+
+$category = [
+    'name' =>$request['name'],
+    'id'=>$request['id']
+];
+
+
+update($category);
+redirectHome();
+?>
